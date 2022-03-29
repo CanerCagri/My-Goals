6524feedb66435ee1aeb2f9f303d507b220197e5
CanerCagri <canerrcagrii@gmail.com>
2022-03-29 20:10:40 +0300

    init

---
+++ My-Goals/My-Goals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-Goals
+//
+//  Created by Caner Çağrı on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
