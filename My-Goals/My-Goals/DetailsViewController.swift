e54a5a6e0b5e62cca80c2fcf8110b55878f1f7f5
CanerCagri <canerrcagrii@gmail.com>
2022-03-29 20:40:40 +0300

    Ui created

---
+++ My-Goals/My-Goals/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  My-Goals
+//
+//  Created by Caner Çağrı on 29.03.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet var goalTextField: UITextField!
+    
+    @IBOutlet var explainTextView: UITextView!
+    
+    @IBOutlet var picker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    @IBAction func saveBtnTapped(_ sender: Any) {
+    }
+    
+
+}
--- My-Goals/My-Goals/ViewController.swift
+++ My-Goals/My-Goals/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightBarButtonTapped))
     }
-
-
+    
+    @objc func rightBarButtonTapped() {
+        performSegue(withIdentifier: "toDetailsVC", sender: self)
+    }
+                                                            
+                                                            
+        
 }
 
