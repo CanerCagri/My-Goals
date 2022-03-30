7a6000d17352923d788bae463923634ba30424d8
CanerCagri <canerrcagrii@gmail.com>
2022-03-30 09:01:28 +0300

    Goal adding ui updated

--- My-Goals/My-Goals/DetailsViewController.swift
+++ My-Goals/My-Goals/DetailsViewController.swift
@@ -15,15 +15,85 @@ class DetailsViewController: UIViewController {
     
     @IBOutlet var picker: UIPickerView!
     
+    let pickerValues = ["5", "4", "3", "2", "1"]
+   
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.dataSource = self
+        picker.delegate = self
+        explainTextView.delegate = self
+        
+        explainTextView.textColor = UIColor.darkGray
+        
+        
 
+        
+       
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+   
+    @IBAction func saveBtnTapped(_ sender: Any) {
+        
        
     }
     
 
-    @IBAction func saveBtnTapped(_ sender: Any) {
+}
+
+extension DetailsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
+    //row count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return pickerValues[row]
+        
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        let pickerLabel = UILabel()
+            let titleData = pickerValues[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedString.Key.font:UIFont(name: "Georgia", size: 26.0)!,NSAttributedString.Key.foregroundColor:UIColor.black])
+            pickerLabel.attributedText = myTitle
+        
+        let hue = CGFloat(row)/CGFloat(pickerValues.count)
+        pickerLabel.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness:1.0, alpha: 1.0)
+        pickerLabel.textAlignment = .center
+        
+        return pickerLabel
+    }
+    
+}
+extension DetailsViewController : UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if (explainTextView.text == "What You Wanna Do For Success Your Goal")
+            {
+                explainTextView.text = nil
+                explainTextView.textColor = UIColor.darkGray
+            }
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        if explainTextView.text.isEmpty
+            {
+                explainTextView.text = "What You Wanna Do For Success Your Goal"
+                explainTextView.textColor = UIColor.darkGray
+            }
+            textView.resignFirstResponder()
+    }
 }
