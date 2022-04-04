bdfb51fb4c61a6cd9de7c5847fa2a02d588e21c1
CanerCagri <canerrcagrii@gmail.com>
2022-04-04 18:11:05 +0300

    CoreData implemented , ui changes

--- My-Goals/My-Goals/DetailsViewController.swift
+++ My-Goals/My-Goals/DetailsViewController.swift
@@ -6,80 +6,58 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController {
 
     @IBOutlet var goalTextField: UITextField!
-    
     @IBOutlet var explainTextView: UITextView!
     
-    @IBOutlet var picker: UIPickerView!
-    
-    let pickerValues = ["5", "4", "3", "2", "1"]
-   
-    
-    
-    
+    var selectedItemName = ""
+    var selectedUUID : UUID?
+    var selectedPriority = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        picker.dataSource = self
-        picker.delegate = self
         explainTextView.delegate = self
-        
         explainTextView.textColor = UIColor.darkGray
-        
-        
-
-        
-       
     }
     override func viewDidAppear(_ animated: Bool) {
         
     }
-   
+    @IBAction func `switch`(_ sender: UISwitch) {
+        
+        if sender.isOn == true {
+            selectedPriority = true
+        } else {
+            selectedPriority = false
+        }
+    }
+    
     @IBAction func saveBtnTapped(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = (appDelegate?.persistentContainer.viewContext)!
+        let goals = NSEntityDescription.insertNewObject(forEntityName: "Goals", into: context)
         
-       
+        goals.setValue(goalTextField.text!, forKey: "goal")
+        goals.setValue(explainTextView.text!, forKey: "goalDescription")
+        goals.setValue(selectedPriority, forKey: "priority")
+        
+        do {
+            try context.save()
+            print("Save completed!")
+        }catch {
+            print("Error , didnt save!")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name("dataEntered"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
-    
-
 }
 
-extension DetailsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    //row count
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerValues.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
-        return pickerValues[row]
-        
-        
-    }
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        
-        let pickerLabel = UILabel()
-            let titleData = pickerValues[row]
-        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedString.Key.font:UIFont(name: "Georgia", size: 26.0)!,NSAttributedString.Key.foregroundColor:UIColor.black])
-            pickerLabel.attributedText = myTitle
-        
-        let hue = CGFloat(row)/CGFloat(pickerValues.count)
-        pickerLabel.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness:1.0, alpha: 1.0)
-        pickerLabel.textAlignment = .center
-        
-        return pickerLabel
-    }
-    
-}
 extension DetailsViewController : UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
-        if (explainTextView.text == "What You Wanna Do For Success Your Goal")
+        if (explainTextView.text == "For Success Your Goal")
             {
                 explainTextView.text = nil
                 explainTextView.textColor = UIColor.darkGray
@@ -88,12 +66,12 @@ extension DetailsViewController : UITextViewDelegate {
     }
     
     func textViewDidEndEditing(_ textView: UITextView) {
-        
         if explainTextView.text.isEmpty
             {
-                explainTextView.text = "What You Wanna Do For Success Your Goal"
+                explainTextView.text = "For Success Your Goal"
                 explainTextView.textColor = UIColor.darkGray
             }
-            textView.resignFirstResponder()
+        textView.resignFirstResponder()
     }
+  
 }
--- My-Goals/My-Goals/ViewController.swift
+++ My-Goals/My-Goals/ViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import CoreData
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var goalArray : [String] = []
+    var descriptionArray : [String] = []
+    var idArray : [UUID] = []
+    var priorityArray : [Bool] = []
+    
+    var selectedName = ""
+    var selectedUUID : UUID?
 
     @IBOutlet var tableView: UITableView!
     
@@ -15,13 +23,65 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightBarButtonTapped))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        loadData()
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(loadData), name: NSNotification.Name(rawValue: "dataEntered") , object: nil)
+    }
+    @objc func loadData() {
+        goalArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        priorityArray.removeAll(keepingCapacity: false)
+        descriptionArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Goals")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let goal = result.value(forKey: "goal") as? String {
+                        goalArray.append(goal)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                    if let description = result.value(forKey: "goalDescription") as? String {
+                        descriptionArray.append(description)
+                    }
+                    if let priority = result.value(forKey: "priority") as? Bool {
+                        priorityArray.append(priority)
+                    }
+                }
+                tableView.reloadData()
+            }
+            
+            
+        } catch {
+            print("Get error when loading data")
+        }
+    }
     @objc func rightBarButtonTapped() {
+        selectedName = ""
         performSegue(withIdentifier: "toDetailsVC", sender: self)
     }
-                                                            
-                                                            
-        
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return goalArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = goalArray[indexPath.row]
+        return cell
+    }
 }
 
